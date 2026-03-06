6de3e4ea6510d9adb670d663a8810f18919f050d
Huzaifa Rafique <huzaifarafique891@gmail.com>
2026-03-06 23:39:59 +0500

    feat: implemented login system

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello Secure Software Development") 
+def login(username, password):
+    if username == "admin" and password == "1234":
+        print("Login successful")
+    else:
+        print("Invalid credentials")
+
+login("admin", "1234")
